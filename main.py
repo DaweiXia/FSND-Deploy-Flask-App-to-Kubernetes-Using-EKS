828cfa5a22f945c5251ff6fd205c17c7b13cceec
xiadw <xiadw_public@163.com>
2020-01-31 17:04:10 +0800

    add describe cluster inline policy

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy Backend!")
+    return jsonify("Add DescribeCluster inline policy. Healthy Backend!")
 
 
 @APP.route('/auth', methods=['POST'])
