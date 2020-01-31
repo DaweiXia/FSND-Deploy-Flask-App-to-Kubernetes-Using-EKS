8a00e6fa643bd94f82d81323f0084afcd62fd339
xiadw <xiadw_public@163.com>
2020-01-31 17:28:01 +0800

    add policy to user not role

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Add DescribeCluster inline policy. Healthy Backend!")
+    return jsonify("Add DescribeCluster inline policy to user. Healthy Backend!")
 
 
 @APP.route('/auth', methods=['POST'])
